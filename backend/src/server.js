70391abdcea5ecb5225af7397c3912fc2a254f89
sawankumar543 <devnagricode@gmail.com>
2026-06-11 19:42:40 +0530

    deployment config added

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,21 +1,33 @@
 import express from 'express';
 import { ENV } from './lib/env.js';
+import path from 'path';
+import { cwd } from 'process';
 
 // make app
 const app = express();
 
-app.get('/', (req, res) => {
-    res.status(200).json({
-        success: true,
-        msg: "Success from backend"
-    })
-})
+const __dirname = path.resolve();
+
 app.get('/home', (req, res) => {
     res.status(200).json({
         success: true,
         msg: "Success from backend home"
     })
 })
+app.get('/books', (req, res) => {
+    res.status(200).json({
+        success: true,
+        msg: "Success from backend books"
+    })
+})
+
+// make ready for deployment
+if(ENV.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, '../frontend', 'dist')))
+    app.get('/{*any}', (req, res) => {
+        res.sendFile(path.join(__dirname, '../frontend', 'dist', 'index.html'))
+    })
+}
 
 // run the server
 app.listen(ENV.PORT, () => {
